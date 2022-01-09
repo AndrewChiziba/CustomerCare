beec093e1330c5336c040e0cf4283b52102797c5
Andrew Chiziba <chizibaandrew@gmail.com>
2022-01-09 02:35:57 +0200

    intCommit

--- Data/Migrations/20220109003302_initMig.Designer.cs
+++ Data/Migrations/20220109003302_initMig.Designer.cs
@@ -4,14 +4,16 @@ using CustomerCare.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CustomerCare.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220109003302_initMig")]
+    partial class initMig
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
